51f1e79cbb00a8238f159ddb5d6e3ee3d74809e6
IN1DM3B_02 <IN1DM3B_02@H015112.NET>
2019-01-17 12:32:34 +0100

    Ventana bienvenida creada 1.0

---
+++ src/main/java/interfaz/Bienvenida.java
@@ -0,0 +1,38 @@
+package interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import java.awt.Font;
+
+public class Bienvenida extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+
+	/**
+	 * Create the frame.
+	 */
+	public Bienvenida() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1280, 720);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		addWindowListener((WindowListener) this);
+	}
+}
