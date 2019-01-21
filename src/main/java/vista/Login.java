349a3d479a0fb54a82102742114ea6473b15e948
IN1DM3B_02 <IN1DM3B_02@H015112.NET>
2019-01-21 12:32:16 +0100

    Panel Registro y Panel Login creados

---
+++ src/main/java/vista/Login.java
@@ -0,0 +1,64 @@
+package vista;
+
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.JComboBox;
+import javax.swing.JTable;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import javax.swing.JScrollBar;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JInternalFrame;
+import java.awt.FlowLayout;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
+public class Login extends JPanel {
+	public JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Create the panel.
+	 */
+	public Login() {
+		setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuario (DNI)");
+		lblUsuario.setHorizontalAlignment(SwingConstants.LEFT);
+		lblUsuario.setBounds(98, 261, 70, 20);
+		add(lblUsuario);
+		
+		textField = new JTextField();
+		textField.setBounds(178, 261, 216, 20);
+		add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPassword.setBounds(108, 292, 53, 14);
+		add(lblPassword);
+		
+		JLabel lblInicioDeSesin = new JLabel("INICIO DE SESI\u00D3N");
+		lblInicioDeSesin.setFont(new Font("Mistral", Font.PLAIN, 44));
+		lblInicioDeSesin.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInicioDeSesin.setBounds(0, 36, 475, 81);
+		add(lblInicioDeSesin);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(178, 292, 216, 14);
+		add(passwordField);
+		
+		JButton btnNewButton = new JButton("Iniciar Sesi\u00F3n");
+		btnNewButton.setBounds(178, 327, 114, 42);
+		add(btnNewButton);
+
+	}
+}
---
+++ src/main/java/vista/SelecLinea.java
@@ -0,0 +1,17 @@
+package vista;
+
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JSlider;
+import javax.swing.JProgressBar;
+
+public class SelecLinea extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public SelecLinea() {
+		setLayout(null);
+
+	}
+}
