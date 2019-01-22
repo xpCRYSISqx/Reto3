f98af4220b5898005521ef928dfa844eaba2a169
laurimoreni <laura.morenogo@elorrieta-errekamari.com>
2019-01-22 08:48:22 +0100

    creados los archivos para las clases

--- src/main/java/modelo/Modelo.java
+++ src/main/java/modelo/Modelo.java
@@ -22,32 +22,57 @@ public class Modelo {
 		ResultSet result = null;
 		ArrayList<Linea> lineas = new ArrayList<Linea>();
 
-		// preparamos la consulta SQL a la base de datos
 		try {
+			
+			// preparamos la consulta SQL a la base de datos
 			stmt = this.connection.prepareStatement("SELECT * FROM linea");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		// Ejecuta la consulta y guarda los resultados en un objeto ResultSet              
-		try {
+			
+			// Ejecuta la consulta y guarda los resultados en un objeto ResultSet   
 			result = stmt.executeQuery();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		try {
+			
+			// crea objetos linea con los resultados y los añade a un arrayList
 			while (result.next()) {
 				this.linea = new Linea();
 				this.linea.setCodLinea(result.getString("Cod_Linea"));
 				this.linea.setNombre(result.getString("Nombre"));
 				lineas.add(this.linea);
 			}
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} 
-
+		}           
+		
 		return lineas;
 		
 	}
+	
+//	public ArrayList<Linea> getParadas() {
+//		
+//		PreparedStatement stmt = null;
+//		ResultSet result = null;
+//		ArrayList<Linea> lineas = new ArrayList<Linea>();
+//
+//		try {
+//			
+//			// preparamos la consulta SQL a la base de datos
+//			stmt = this.connection.prepareStatement("SELECT * FROM linea");
+//			
+//			// Ejecuta la consulta y guarda los resultados en un objeto ResultSet   
+//			result = stmt.executeQuery();
+//			
+//			// crea objetos linea con los resultados y los añade a un arrayList
+//			while (result.next()) {
+//				this.linea = new Linea();
+//				this.linea.setCodLinea(result.getString("Cod_Linea"));
+//				this.linea.setNombre(result.getString("Nombre"));
+//				lineas.add(this.linea);
+//			}
+//			
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}           
+//		
+//		return lineas;
+//		
+//	}
 }
