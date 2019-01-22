b8a9610b018765b74c76ef584dd720b078b76055
unknown <Elorrieta Errekamari@INFP4A101-04.H015112.NET>
2019-01-22 09:53:07 +0100

    Clases cliente y Encriptacion creadas

--- src/main/java/modelo/Cliente.java
+++ src/main/java/modelo/Cliente.java
@@ -1,6 +1,15 @@
 package modelo;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.Date;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Cliente {
 	
@@ -9,5 +18,68 @@ public class Cliente {
 	private String apellidos;
 	private Date fechaNacimiento;
 	private char sexo;
+	private String contraseña;
+	
+	public Cliente() {
+		
+	}
+	
+	public Cliente(String dni, String nombre, String apellidos, Date fechaNacimiento, char sexo, String contraseña) {
+		
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechaNacimiento = fechaNacimiento;
+		this.sexo = sexo;
+		this.contraseña = contraseña;
+	}
 
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	
 }
