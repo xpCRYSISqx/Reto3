33ade5741e5c8bd1527935a353d4cd2790c53648
uztaOna <uztaritz.buenomo@elorrieta-errekamari.com>
2019-02-13 13:52:57 +0100

    tests de constructores OK

--- src/test/java/modeloT/TestLinea.java
+++ src/test/java/modeloT/TestLinea.java
@@ -1,14 +1,37 @@
 package modeloT;
 
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import org.junit.Test;
+import modelo.*;
 
 public class TestLinea {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructorVacioLinea() {
+		//Arrange
+		Linea linea = new Linea();
+				
+		//Assert
+		assertEquals(linea.getCodLinea(),null);
+		assertEquals(linea.getNombre(),null);
+		assertEquals(linea.getAutobuses(),null);
+		assertEquals(linea.getMunicipios(),null);
+	}
+	
+	@Test
+	public void testConstructorLinea() {
+		//Arrange
+		ArrayList<Autobus> autobuses = new ArrayList<Autobus>();
+		ArrayList<Municipio> municipios = new ArrayList<Municipio>();
+		
+		Linea linea = new Linea("L66","Zeca-Meca", autobuses, municipios);
+				
+		//Assert
+		assertEquals(linea.getCodLinea(),"L66");
+		assertEquals(linea.getNombre(),"Zeca-Meca");
+		assertEquals(linea.getAutobuses(),autobuses);
+		assertEquals(linea.getMunicipios(),municipios);
 	}
 
 }
