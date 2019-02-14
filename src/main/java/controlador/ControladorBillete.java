ff468f1c8516d1294489285445fb0028ae7ab1b6
laurimoreni <laura.morenogo@elorrieta-errekamari.com>
2019-02-14 08:22:45 +0100

    validado que las paradas de origen y destino no sean la misma

--- src/main/java/controlador/ControladorBillete.java
+++ src/main/java/controlador/ControladorBillete.java
@@ -145,6 +145,12 @@ public class ControladorBillete implements ActionListener {
 			this.paradaDestino = (Parada) vista.sel_billete.listaDestino.getSelectedValue();
 		}
 		
+		// comprobar que la parada de origen y destino no sean la misma
+		if (this.paradaOrigen.getCodParada() == this.paradaDestino.getCodParada()) {
+			JOptionPane.showMessageDialog(vista, "Las paradas de origen y destino no pueden ser iguales", "Aviso", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+		
 		// guardar tipo de billete
 		if (!vista.sel_billete.rbtnIda.isSelected() && !vista.sel_billete.rbtnVuelta.isSelected()) {
 			JOptionPane.showMessageDialog(vista, "Ningun tipo de billete seleccionado.", "Aviso", JOptionPane.WARNING_MESSAGE);
